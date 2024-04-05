2166f3c8686de612f9cc8880a943ff2a349d0343
Ukjong <xb253q@gmail.com>
2024-04-05 21:43:23 +0900

    lombok세팅 및 실습

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(HTML 파일)
@@ -14,7 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 // 사용자가 요청 -> 응답(Data)
 @RestController
 public class HttpControllerTest {
+    private static final String TAG = "HttpControllerTest: ";
 
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        Member m1 = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+        System.out.println(TAG + "getter: " + m1.getUsername());
+        m1.setUsername("cos");
+        System.out.println(TAG + "getter: " + m1.getUsername());
+        return "lombok test완료";
+    }
     // 인터넷 브라우저 요청은 get만 가능
     //http://localhost:8080/http/get (select)
     @GetMapping("/http/get")

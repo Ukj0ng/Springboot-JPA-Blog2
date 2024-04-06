5f0d1141b7afb7a4bc9decade9f6c22ff7fd8828
Ukjong <xb253q@gmail.com>
2024-04-06 20:24:34 +0900

    refactor: 사용하지 않는 라이브러리 제거

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -2,12 +2,12 @@ package com.cos.blog.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,7 +32,7 @@ public class Reply {
     @JoinColumn(name = "boardId")
     private Board board;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "userId")
     private User user; // 객체가 정해져 알아서 따라갈 수 있음
 

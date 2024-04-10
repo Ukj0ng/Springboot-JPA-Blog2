20bd2b10587491b48dc85e721cb08397b85d3c28
Ukjong <xb253q@gmail.com>
2024-04-10 11:13:15 +0900

    feat: User DB 추가

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.cos.blog.repository;public interface UserRepository {
+package com.cos.blog.repository;
+
+import com.cos.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// DAO
+// 자동으로 bean으로 등록이 됨. -> @Repository 생략 가능
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
